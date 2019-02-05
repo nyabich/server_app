5cbee78affc11adf4ea046d1c1527bb8895c2ef8
nyabich <pascaldamiennyassanoah@gmail.com>
2019-02-05 11:17:24 +0100

    Revert "premier commit - on revient à l'état initial"
    This reverts commit 38afc0fcdb6a39cdcb2d6f3156bb6a66450b198d.

--- server/controllers/index.js
+++
@@ -1,9 +0,0 @@
-const client = require('./ClientController');
-const gestionnaire = require('./GestionnaireController');
-const commande =  require('./CommandController');
-
-module.exports = {
-  client,
-  gestionnaire,
-  commande
-};
